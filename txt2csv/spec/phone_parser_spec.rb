a243bd3243ad794652245b91c8cf97fe1074f318
JinLee <jinlee0908@gmail.com>
2014-02-15 10:23:20 -0800

    start of phone tests

--- txt2csv/spec/phone_parser_spec.rb
+++ txt2csv/spec/phone_parser_spec.rb
@@ -1,3 +1,45 @@
 require "spec_helper"
 
 require "parse.rb"
+
+require "spec_helper"
+
+require "parse.rb"
+
+describe Parse do
+  it "should parse number without country code" do   
+  return_array = Parse.parse_number("345-234-1244-x342")
+  expect(return_array).to eq(["","345","234","1244","342"])
+  end
+
+  it "should parse number without an ext" do   
+    return_array = Parse.parse_numbers("1-345-234-1244")
+    expect(return_array).to eq(["1","345","234","1244",""])
+  end
+
+  it "should parse number without an ext" do   
+    return_array = Parse.parse_numbers("1-345-234-1244")
+    expect(return_array).to eq(["1","345","234","1244",""])
+  end
+
+  it "should parse number without country code or ext" do   
+    return_array = Parse.parse_numbers("345-234-1244")
+    expect(return_array).to eq(["","345","234","1244",""])
+  end
+
+  it "should parse number with () as a separator" do   
+    return_array = Parse.parse_numbers("(345)234-1244")
+    expect(return_array).to eq(["","345","234","1244",""])
+  end
+
+  it "should parse number with . as a separator" do   
+    return_array = Parse.parse_numbers("1.345.234.1244.x345")
+    expect(return_array).to eq(["1","345","234","1244","345"])
+  end
+
+  it "should parse number with space as a separator" do   
+    return_array = Parse.parse_numbers("1 345 234 1244 x345")
+    expect(return_array).to eq(["1","345","234","1244","345"])
+  end
+
+end

5545e0261c502c6712176747b4fc52a54ec90b45
JinLee <jinlee0908@gmail.com>
2014-02-18 12:34:23 -0800

    tweaks due to rubocop

--- txt2csv/spec/twitter_parser_spec.rb
+++ txt2csv/spec/twitter_parser_spec.rb
@@ -1,49 +1,49 @@
-require "spec_helper"
+require 'spec_helper'
 
-require "parse.rb"
+require 'parse.rb'
 
-#Output the parsed data as an array of strings, just as with the names.
-## twitter name
-#There are two forms:
-#* @_string_
-#* _string_
+# Output the parsed data as an array of strings, just as with the names.
+# # twitter name
+# There are two forms:
+# * @_string_
+# * _string_
 
-#For example, 
-#* @poohbear
-#* tigger
+# For example,
+# * @poohbear
+# * tigger
 
-#Strip the ampersand if present. Output the result as a single element array. 
-#(Note that all the parsing methods return an array, even if it's just one element. 
-#Why do you think this is?)
+# Strip the ampersand if present. Output the result as a single element array.
+# (Note that all the parsing methods return an array,
+# even if it's just one element. Why do you think this is?)
 
 describe Parse do
-   it "should accept twitter handles without ampersand" do   
-    return_array = Parse.parse_twitter( "pluto")
-    expect(return_array).to eq(["pluto"])
+  it "should accept twitter handles without ampersand" do
+    return_array = Parse.parse_twitter('pluto')
+    expect(return_array).to eq(['pluto'])
   end
 
-	 it "should remove amersand" do   
-    return_array = Parse.parse_twitter( "@tiger")
-    expect(return_array).to eq(["tiger"])
+  it "should remove amersand" do
+    return_array = Parse.parse_twitter('@tiger')
+    expect(return_array).to eq(['tiger'])
   end
 
-  it "should accept words with underscores" do   
-    return_array = Parse.parse_twitter( "@winnie_the_pooh")
-    expect(return_array).to eq(["winnie_the_pooh"])
+  it "should accept words with underscores" do
+    return_array = Parse.parse_twitter('@winnie_the_pooh')
+    expect(return_array).to eq(['winnie_the_pooh'])
   end
 
-  it "should accept words with mixed case letters" do   
-    return_array = Parse.parse_twitter( "@ScroogeMcDuck")
-    expect(return_array).to eq(["ScroogeMcDuck"])
+  it "should accept words with mixed case letters" do
+    return_array = Parse.parse_twitter('@ScroogeMcDuck')
+    expect(return_array).to eq(['ScroogeMcDuck'])
   end
 
-  it "should accept letters and numbers" do   
-    return_array = Parse.parse_twitter( "@I3U")
-    expect(return_array).to eq(["I3U"])
+  it "should accept letters and numbers" do
+    return_array = Parse.parse_twitter('@I3U')
+    expect(return_array).to eq(['I3U'])
   end
 
-  it "should accept only numbers" do   
-    return_array = Parse.parse_twitter( "1234")
-    expect(return_array).to eq(["1234"])
+  it "should accept only numbers" do
+    return_array = Parse.parse_twitter('1234')
+    expect(return_array).to eq(['1234'])
   end
 end
